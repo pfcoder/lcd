667f1ba7092372d16628e9b418598fd7151cd2a7
chris <chris.zhu@ott.ca>
2024-03-28 17:38:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from "./components/Greet.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Welcome to LCD(Liquid Cooler Desktop)!</h1>
+
+    <Greet />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
